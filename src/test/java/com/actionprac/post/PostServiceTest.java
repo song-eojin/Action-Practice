8b141886ea3cf35662c5e693b807b9f4a4c64096
JisooPyo <audwl326@gmail.com>
2023-08-05 23:42:14 +0900

    테스트 코드 틀 작성

---
+++ src/test/java/com/actionprac/post/PostServiceTest.java
@@ -0,0 +1,24 @@
+package com.actionprac.post;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// requestDto로 만든 Post의 값과 저장된 Post의 값이 일치하는지
+// 테스트 코드 작성
+class PostServiceTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	void createPost() {
+	}
+}
