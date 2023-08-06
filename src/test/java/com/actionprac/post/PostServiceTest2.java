ce864d594b19435617148889d3016543550ed8a6
JisooPyo <audwl326@gmail.com>
2023-08-06 17:09:53 +0900

    스프링부트테스트 일단 됨..

--- src/test/java/com/actionprac/post/PostServiceTest2.java
+++ src/test/java/com/actionprac/post/PostServiceTest2.java
@@ -11,36 +11,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//@Transactional // 롤백시키기 위한 어노테이션
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Transactional // 롤백시키기 위한 어노테이션
 class PostServiceTest2 {
 
-//	@Autowired
-//	PostRepository postRepository;
-//	@Autowired
-//	PostService postService;
-//
-//	// requestDto로 만든 Post의 값과 저장된 Post의 값이 일치하는지
-//	// 테스트 코드 작성
-//
-//	@Test
-//	@DisplayName("createPost() 테스트")
-//	void createPost() {
-//		// given
-//		String content = "test_content";
-//
-//		PostRequestDto requestDto = new PostRequestDto();
-//		requestDto.setContent(content);
-//
-//		Post post = new Post(requestDto);
-//
-//		// when
-//		PostResponseDto responseDto = postService.createPost(requestDto);
-//
-//		// then
-//		assertEquals(responseDto.getContent(),content);
-//		assertNotNull(responseDto.getId());
-//
-//	}
+	@Autowired
+	PostRepository postRepository;
+	@Autowired
+	PostService postService;
+
+	// requestDto로 만든 Post의 값과 저장된 Post의 값이 일치하는지
+	// 테스트 코드 작성
+	@Test
+	@DisplayName("createPost() 테스트")
+	void createPost() {
+		// given
+		String content = "test_content";
+
+		PostRequestDto requestDto = new PostRequestDto();
+		requestDto.setContent(content);
+
+		Post post = new Post(requestDto);
+
+		// when
+		PostResponseDto responseDto = postService.createPost(requestDto);
+
+		// then
+		assertEquals(responseDto.getContent(),content);
+		assertNotNull(responseDto.getId());
+
+	}
 }
