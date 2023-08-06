b6ddedd47f5bbec5235bb611d3ebdb0e4721d152
JisooPyo <audwl326@gmail.com>
2023-08-06 14:46:15 +0900

    테스트 코드 수정 @Mock으로

--- src/test/java/com/actionprac/post/PostServiceTest.java
+++ src/test/java/com/actionprac/post/PostServiceTest.java
@@ -2,19 +2,23 @@ package com.actionprac.post;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.BDDMockito.given;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(MockitoExtension.class)
 @Transactional // 롤백시키기 위한 어노테이션
 class PostServiceTest {
 
-	@Autowired
+	@Mock
 	PostRepository postRepository;
 
 	// requestDto로 만든 Post의 값과 저장된 Post의 값이 일치하는지
@@ -23,6 +27,7 @@ class PostServiceTest {
 	@Test
 	@DisplayName("createPost() 테스트")
 	void createPost() {
+		// given
 		String content = "test_content";
 
 		PostRequestDto requestDto = new PostRequestDto();
@@ -30,15 +35,13 @@ class PostServiceTest {
 
 		Post post = new Post(requestDto);
 
+		PostService postService = new PostService(postRepository);
+
 		// when
-		postRepository.save(post);
-		PostResponseDto responseDto = new PostResponseDto(post);
+		PostResponseDto responseDto = postService.createPost(requestDto);
 
 		// then
-		assertNotNull(responseDto.getId());
-		System.out.println("notnull 통과");
+		assertEquals(responseDto.getContent(),content);
 
-		assertEquals(content, responseDto.getContent());
-		System.out.println("assertEquals 통과");
 	}
 }
